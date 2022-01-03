f1db3899ccd417d712dbfb7eeeda0de7a14da8fb
rym49 <hatanakaryoma.130@gmail.com>
2022-01-03 14:52:24 +0900

    修正

--- FactoryMethod/Main.java
+++ FactoryMethod/Main.java
@@ -1,14 +1,13 @@
 package FactoryMethod;
 
-import FactoryMethod.Package.Framework.*;
 import FactoryMethod.Package.IdCard.*;
 
 class Main {
   public static void main(String[] args) {
-    Factory factory = new IdCardFactory();
-    Product p1 = factory.create("taro");
-    Product p2 = factory.create("jiro");
-    Product p3 = factory.create("saburo");
+    IdCardFactory factory = new IdCardFactory();
+    IdCard p1 = (IdCard)factory.create("taro");
+    IdCard p2 = (IdCard)factory.create("jiro");
+    IdCard p3 = (IdCard)factory.create("saburo");
 
     p1.use();
     p2.use();
